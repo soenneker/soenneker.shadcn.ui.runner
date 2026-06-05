50e2d7e7dec26b74ccafb4af0ff77e2ba9e108a7
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:37:32 -0500

    xml commenting

--- src/Soenneker.Shadcn.Ui.Runner/Constants.cs
+++ src/Soenneker.Shadcn.Ui.Runner/Constants.cs
@@ -1,10 +1,28 @@
 namespace Soenneker.Shadcn.Ui.Runner;
 
+/// <summary>
+/// Represents the constants.
+/// </summary>
 public static class Constants
 {
+    /// <summary>
+    /// The shadcn ui repository.
+    /// </summary>
     public const string ShadcnUiRepository = "https://github.com/shadcn-ui/ui.git";
+    /// <summary>
+    /// The crawled repository.
+    /// </summary>
     public const string CrawledRepository = "https://github.com/soenneker/soenneker.shadcn.ui.crawled.git";
+    /// <summary>
+    /// The components repository.
+    /// </summary>
     public const string ComponentsRepository = "https://github.com/soenneker/soenneker.shadcn.ui.components.git";
+    /// <summary>
+    /// The components url.
+    /// </summary>
     public const string ComponentsUrl = "https://ui.shadcn.com/docs/components";
+    /// <summary>
+    /// The commit message.
+    /// </summary>
     public const string CommitMessage = "Refresh shadcn component preview HTML";
 }
